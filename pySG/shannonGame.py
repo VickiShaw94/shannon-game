d718d4861546b3da30b53df394f315d4619abb03
Vicki Shaw <vickishaw94@gmail.com>
2017-06-04 17:08:56 -0400

    adding in Electron functionality

---
+++ pySG/shannonGame.py
@@ -0,0 +1,69 @@
+import random
+
+
+##TODO: add in phrases/dictionary module of words
+
+def random_word():
+    """
+    randomly selects a word
+    :return: string random_word
+    """
+    words = ['Shakespeare', 'sound', 'and', 'fury']
+    return random.choice(words).upper()
+
+
+def guess_check(word):
+    """
+    iterates through each letter in the word, checks against user prompted guess
+    stores items in a dictionary
+    :param word: random word selected to be guessed
+    """
+
+    # initializes empty guess string
+    progressStr = ["_  "] * len(word)
+    tryNum = []
+
+    print " " .join(progressStr)
+
+    for i, s in enumerate(word):
+        guessNum = 0
+        guessed = False
+        guesses = [] # letters guessed
+
+        while not guessed:
+            guess = raw_input("\n").upper()
+            print " " .join(progressStr)
+
+            if len(guess) > 1:
+                print "Please enter one letter at a time."
+
+            elif len(guess) == 0:
+                print "Please guess one letter"
+
+            # if valid input, but not the correct letter
+            elif (guess != s):
+                if guess not in guesses:
+                    guessNum += 1
+                    guesses.append(guess)
+                    print "Guess again! \nGuess count:", guessNum
+
+                else:
+                    print "You already guessed that! Try again"
+
+            else: # correct letter guessed
+                guessed = True
+                tryNum.append(guessNum + 1)
+                progressStr[i] = s
+                print "Good guess! \n" + ", ".join(map(str, tryNum))
+                print "  " .join(progressStr)
+
+def shannon():
+    word = random_word()
+    print "This word contains", len(word), "letters. Start guessing! \n"
+    guess_check(word)
+
+
+shannon()
+
+
+
