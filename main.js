9b47bba3366192c09e46722fdfa78891b1eb7207
Vicki Shaw <vickishaw94@gmail.com>
2017-06-08 22:46:29 -0400

    dereference window object

--- main.js
+++ main.js
@@ -59,35 +59,3 @@ app.on('activate', function () {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
-/*
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
-const path = require('path')
-
-let mainWindow = null
-const createWindow = () => {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-  mainWindow.loadURL(require('url').format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-  mainWindow.webContents.openDevTools()
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
-}
-app.on('ready', createWindow)
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-*/
