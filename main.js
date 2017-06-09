85fcfb96e3621d3b317181dc269f6e389eb433c8
Vicki Shaw <vickishaw94@gmail.com>
2017-06-08 22:44:27 -0400

    dereference window object

--- main.js
+++ main.js
@@ -29,8 +29,7 @@ function createWindow () {
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+    // in an array if app supports multi windows
     mainWindow = null
   })
 }
